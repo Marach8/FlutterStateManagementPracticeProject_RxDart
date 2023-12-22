7d84ab4af1c9b5fcf405c2e1c9d46f261dae7a2b
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2023-12-22 13:49:35 +0100

    Deeper dive into concatWith operator

--- rxdart_practice_course/lib/main.dart
+++ rxdart_practice_course/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rxdart_practice_course/rx_example1/example1.dart';
+import 'package:rxdart_practice_course/rx_example10/example10.dart';
 import 'package:rxdart_practice_course/rx_example2/views/home_view.dart';
 import 'package:rxdart_practice_course/rx_example3/example3.dart';
 import 'package:rxdart_practice_course/rx_example4/example4.dart';
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         unselectedWidgetColor: Colors.yellow
       ),
-      home: const Example9()
+      home: const Example10()
     );
   }
 }
--- rxdart_practice_course/lib/rx_example10/example10.dart
+++ rxdart_practice_course/lib/rx_example10/example10.dart
@@ -1,4 +1,21 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:rxdart/rxdart.dart';
+
+
+Stream<String> getName(String filePath){
+  final names = rootBundle.loadString(filePath);
+  return Stream.fromFuture(names).transform(const LineSplitter());
+}
+
+
+Stream<String> getAllNames() => 
+  getName('assets/texts/cats.txt').concatWith([getName('assets/texts/dogs.txt')])
+    .delay(const Duration(seconds:3));
+
+
 
 class Example10 extends StatelessWidget {
   const Example10({super.key});
@@ -7,7 +24,26 @@ class Example10 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('concatWith in RxDart'), centerTitle: true),
-      body: Column()
+      body: FutureBuilder<List<String>>(
+        future: getAllNames().toList(),
+        builder: (context, snapshot){
+          switch(snapshot.connectionState){
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+            case ConnectionState.active:
+              return const Center(child: CircularProgressIndicator());
+            case ConnectionState.done:
+              final names = snapshot.requireData;
+              return ListView.separated(
+                separatorBuilder: (_, __) => const Divider(color: Colors.black),
+                itemCount: names.length,
+                itemBuilder: (_, index){
+                  return ListTile(title: Text(names.elementAt(index)));
+                }
+              );
+          }
+        }
+      )
     );
   }
 }
