bcf86cb988335780952e77159a0e446eec99ae7c
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2023-12-24 15:27:00 +0100

    Done setting up the popup menu view

--- rxdart_practice_course/lib/rx_example11/views/popupview.dart
+++ rxdart_practice_course/lib/rx_example11/views/popupview.dart
@@ -1,15 +1,31 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:rxdart_practice_course/rx_example11/dialogs/error_dialogs.dart';
 import 'package:rxdart_practice_course/rx_example11/extensions/typedefs.dart';
+
+enum MenuAction {logout, deleteAccount}
 
 class PopUpMenuView extends StatelessWidget {
   final LogoutCallback logout; 
   final DeleteAccountCallback deleteAccount;
-  const PopUpMenuView({super.key});
+  const PopUpMenuView({required this.logout, required this.deleteAccount, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return PopupMenuButton(
-      itemBuilder: itemBuilder)
+    return PopupMenuButton<MenuAction>(
+      onSelected: (value) async{
+        switch(value){          
+          case MenuAction.logout:
+            final shouldLogout = await showLogOutDialog(context: context);
+            if (shouldLogout){logout();} break;
+          case MenuAction.deleteAccount:
+            final shouldDeleteAccount = await showDeleteAccountDialog(context: context);
+            if (shouldDeleteAccount){deleteAccount();} break;
+        }
+      },
+      itemBuilder: (context) => [
+        const PopupMenuItem(value: MenuAction.logout, child: Text('Logout'),),
+        const PopupMenuItem(value: MenuAction.deleteAccount, child: Text('Delete Account'),)
+      ]
+    );
   }
 }
